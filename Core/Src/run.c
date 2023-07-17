b414b1554b98ef36e3fd5d06c5655ed51996a003
haliwt <jonewang@126.com>
2023-07-17 10:46:48 +0800

    Update

--- Core/Src/run.c
+++ Core/Src/run.c
@@ -75,8 +75,14 @@ void Run_InputKey_Model(uint8_t keyvalue)
 
 		case KEY_LONG_PRES_CONFIRM_MODE : //long key
             if(led_on_of_number == KEEP_HEAT_LED){
-            tpd_t.confirm_key_select_item_keep_heat == keep_heat_enable; 
-			Key_Confirm_Handler(KEEP_HEAT_LED);
+            if(tpd_t.keep_heat_fun_digital_numbers==1){
+                tpd_t.confirm_key_select_item_keep_heat == keep_heat_enable; 
+                tpd_t.keep_heat_run_flag=0;
+    			tpd_t.keep_heat_fun_digital_numbers=0;
+                KEEP_HEAT_LED_ON();
+                ADD_DEC_LED_OFF();
+    			tpd_t.run_process_tag= KEY_CONFIRM_ITEM;
+            }
 
             }
 
